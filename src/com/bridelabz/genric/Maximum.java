88bcd6b27231ed552cd399726696923982d2fc99
mukeshkapse <mukeshkapse1832@gmail.com>
2022-08-29 12:51:08 +0530

    Added: Find  Float maximum

--- src/com/bridelabz/genric/Maximum.java
+++ src/com/bridelabz/genric/Maximum.java
@@ -1,13 +1,14 @@
 package com.bridelabz.genric;
 
 public class Maximum {
-        public static Integer getmaximum(Integer value1, Integer value2, Integer value3) {
-            Integer max = value1;
-            if (value2.compareTo(value1) > 0 && value2.compareTo(value3) > 0)
-                max = value2;
-            else if (value3.compareTo(value2) > 0 && value3.compareTo(value1) > 0)
-                max = value3;
-            System.out.println(max);
-            return max;
-        }
+    public static Float getmaximum(Float value1, Float value2, Float value3) {
+        Float max = value1;
+        if (value2.compareTo(value1) > 0 && value2.compareTo(value3) > 0)
+            max = value2;
+        else if (value3.compareTo(value2) > 0 && value3.compareTo(value1) > 0)
+            max = value3;
+        System.out.println(max);
+        return max;
     }
+
+}
